1a09ee2043302152bd42cbeecb093e07f8a3b9d1
cgp-client <code.gen.pro.system@gmail.com>
2024-08-23 13:35:29 +0300

    Adding run.sh

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install Node
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Init a project
+mkdir -p /app
+cd /app
+npm init -y
+
+# Install Express
+npm install express
+
+# Start the server
+node app.js
